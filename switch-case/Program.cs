e459a0cdd4129210e05b1e6b45ade94d3ffda493
Ümmühan Keleş <ummuhankeles4@gmail.com>
2021-11-01 16:47:36 +0300

    completed

---
+++ switch-case/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak ayındasınız!");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat ayındasınız!");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan ayındasınız!");
+                    break;
+                case 11:
+                    Console.WriteLine("Kasım ayındasınız!");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış değer girdiniz!");
+                    break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış ayındasınız!");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlknahar ayındasınız!");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz ayındasınız!");
+                    break;
+                default:
+                    Console.WriteLine("Sonbahar ayındasınız!");
+                    break;
+            }
+        }
+    }
+}
