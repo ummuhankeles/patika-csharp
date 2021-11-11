c5ec3548d8f9f75dde41c12ec92ae432f2b50bc1
Ümmühan Keleş <ummuhankeles4@gmail.com>
2021-11-11 22:48:16 +0300

    completed

---
+++ diziler-array-sinifi-metodlari/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace diziler_array_sinifi_metodlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // short
+            int[] sayiDizisi = {23, 12, 5, 34, 26, 9, 10, 85, 71};
+            Console.WriteLine("***** Sırasız Dizi *****");
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            Console.WriteLine("***** Sıralı Dizi *****");
+            Array.Sort(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            // clear 
+            Console.WriteLine("***** Array Clear *****");
+            Array.Clear(sayiDizisi, 2,2);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            // reverse
+            Console.WriteLine("***** Array Reverse *****");
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            // indexOf
+            Console.WriteLine("***** Array Reverse *****");
+            Console.WriteLine(Array.IndexOf(sayiDizisi, 23));
+
+            // resize
+            Console.WriteLine("***** Array Resize *****");
+            Array.Resize<int>(ref sayiDizisi,9);
+            sayiDizisi[8] = 99;
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+        }
+    }
+}
